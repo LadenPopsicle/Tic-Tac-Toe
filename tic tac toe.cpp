78655275fcc546a29a9ea3960fc7454dbac1bc7b
LadenPopsicle <mullingt@bxscience.edu>
2017-06-11 14:12:17 -0400

    Create tic tac toe.cpp

--- tic tac toe.cpp
+++ tic tac toe.cpp
@@ -18,17 +18,17 @@ using namespace std;
 
 int main (){
 
-char board1[4][5] = {{' ', '|', ' ', '|', ' '},
-                             {'-', '+', '-', '+', '-'},
-                             {' ', '|', ' ', '|', ' '},
-                             {'-', '+', '-', '+', '-'}};
+char board1[5][5] = {{' ', '|', ' ', '|', ' '},
+                     {'-', '+', '-', '+', '-'},
+                     {' ', '|', ' ', '|', ' '},
+                     {'-', '+', '-', '+', '-'},
+                     {' ', '|', ' ', '|', ' '}};
 
- for(int i=0; i<sizeof(board1); i++) {
-    for(int j=0; j< sizeof(board1[0]); j++) {
+ for(int i=0; i<sizeof(board1)/sizeof(board1[0]); i++) {
+    for(int j=0; j< sizeof(board1[0])/sizeof(char); j++) {
       cout << board1[i][j];
     }
+    cout <<endl;
  }
 
 }
-
-
